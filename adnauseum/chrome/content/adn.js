0d2d943a6f8fb64faa08aeaa027bcb8622ac5b9e
Daniel C. Howe <daniel.c.howe@gmail.com>
2013-10-02 12:20:52 +0800

    updates

--- adnauseum/chrome/content/adn.js
+++ adnauseum/chrome/content/adn.js
@@ -1,61 +1,76 @@
-if (alert) alert("Running adn.js");
 
-if (dump) dump("Running adn.js");
 
-if (console) console.log("Running adn.js");
+/** AdNauseum namespace         */
+if ("undefined" == typeof(AdNauseum)) {
+  if (typeof dump === 'function') 
+  	dump('\n[ADN] Initializing Namespace');
+  var AdNauseum = {};
+};
 
-var adnUI = {
+AdNauseum.UI = {
 	
     myVariable: "value",
     mySettingOne: false,
     mySettingTwo: true,
 
-    someFunction: function(param1, param2)
+    viewLog: function()
     {
-        // Do something clever here
     },
     
-    anotherFunction: function()
+    viewSnaps: function()
     {
-        // More clever stuff
+    },
+    
+    toggleSnaps: function()
+    {
+    	this.myAlert('d:toggleSnaps');
+    },
+    
+    toggleEnabled: function()
+    {
+    },
+    
+    myAlert: function(param1)
+    {
+		window.alert("\n[ADN] "+param1);
     }
 };
+   
+	/**
+	 * Installs the toolbar button with the given ID into the given
+	 * toolbar, if it is not already present in the document.
+	 *
+	 * @param {string} toolbarId The ID of the toolbar to install to.
+	 * @param {string} id The ID of the button to install.
+	 * @param {string} afterId The ID of the element to insert after. @optional
 
-
-
-/**
- * Installs the toolbar button with the given ID into the given
- * toolbar, if it is not already present in the document.
- *
- * @param {string} toolbarId The ID of the toolbar to install to.
- * @param {string} id The ID of the button to install.
- * @param {string} afterId The ID of the element to insert after. @optional
- */
-function installButton(toolbarId, id, afterId) {
+	 installButton : function(toolbarId, id, afterId) {
+	 	
+		setTimeout(function(){window.alert("[ADN] installButton();")},3000);
+		
+	    if (!document.getElementById(id)) {
+	    	
+	        var toolbar = document.getElementById(toolbarId);
+	        // If no afterId is given, then append the item to the toolbar
+	        var before = null;
+	        if (afterId) {
+	            let elem = document.getElementById(afterId);
+	            if (elem && elem.parentNode == toolbar)
+	                before = elem.nextElementSibling;
+	        }
 	
-    if (!document.getElementById(id)) {
-    	
-        var toolbar = document.getElementById(toolbarId);
-        // If no afterId is given, then append the item to the toolbar
-        var before = null;
-        if (afterId) {
-            let elem = document.getElementById(afterId);
-            if (elem && elem.parentNode == toolbar)
-                before = elem.nextElementSibling;
-        }
-
-        toolbar.insertItem(id, before);
-        toolbar.setAttribute("currentset", toolbar.currentSet);
-        document.persist(toolbar.id, "currentset");
-
-        if (toolbarId == "addon-bar")
-            toolbar.collapsed = false;
-    }
-}
+	        toolbar.insertItem(id, before);
+	        toolbar.setAttribute("currentset", toolbar.currentSet);
+	        document.persist(toolbar.id, "currentset");
+	
+	        if (toolbarId == "addon-bar")
+	            toolbar.collapsed = false;
+	    }
+	}
 
 if (firstRun) {
     // installButton("nav-bar", "my-extension-navbar-button");
     // The "addon-bar" is available since Firefox 4
-    installButton("addon-bar", "adnauseum-button");
-}    
+    AdNauseum.UI.installButton("addon-bar", "adnauseum-button");
+}   */ 
 	

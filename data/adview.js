a406e1f36c58632f8ba8b7a3c2088f8f7f7fd3e1
Daniel Howe <daniel@rednoise.org>
2014-01-18 20:45:02 +0800

    adview now its own page

--- data/adview.js
+++ data/adview.js
@@ -1,70 +1,45 @@
+self.port.on("ADNUpdateAdView", updateAdView);
 
-self.addEventListener('click', function(event) {
- 
-	if (event.button == 2 || (event.button == 0 && event.shiftKey == true))
-		addon.port.emit('right-click');
-		
-	event.preventDefault(); 
-	
-	addon.port.emit("ADNCloseAdView");
-
-}, true);
-
-addon.port.on("ADNUpdateAdView", updateAdView);
+updateAdView(self.options);
 
 function updateAdView(o) {
 	
-	//console.log('AdView::ADNUpdateAdView()');
+	//console.log('AdView::ADNUpdateAdView('+o.ads.length+')');
 	
-	var ads = o.ads, page = o.page, listPages = 1,
-		result = '<p>Current: ' + page + '<br/>\n';
+	var result, ads = o.ads;
 		
-	ads.sort(function(a,b) { 
+	ads.sort(function(a,b) { // sort by found-time
+		
 		return (a.found > b.found) ? 1 : ((b.found > a.found) ? -1 : 0); 
 	});
+
+	result = formatDivs(ads);
 	
-	/*if (listPages) { // list all stored pages
-		for (var i = 0, j = keys.length; i < j; i++) {
-			var ads = adlookup[keys[i]];
-			result += '<b>' + keys[i] + ":" + ads.length + "</b><br/>\n";
-		}
-		result += '</p>\n';
-	}
+	//console.log(result);
+
+	$('#container').html(result);
+	$('#json').html(formatJSON(ads));
 	
-	if (page) { // ads for current page
+	var $container = $('#container');
+	$container.isotope({
+	  itemSelector: '.item',
+	  layoutMode: 'masonry'
+	});
+}
+
+function formatDivs(ads) {
+	
+	var html = '';
+	for (var i=0, j = ads.length; i<j; i++) {
 		
-	}*/
-	
-	result += formatJSON(ads);
-
-	///console.log(result);
-
-	$('#content').html(result);
+		if (ads[i].url) {
+			html += '<div class="item"><img src="' + ads[i].url + '"></div>\n';
+		}
+	}
+	return html;
 }
 
 function formatJSON(data) {
 
 	return JSON.stringify(data, null, 4).replace(/\n/g, "<br/>").replace(/ /g, "&nbsp;");
-}
-
-function formatHTML(data) { // NOT USED RIGHT NOW
-
-	var keys = Object.keys(data), html = '';
-
-	for (var i = 0, j = keys.length; i < j; i++) {
-
-		html += '<div class=adn-page><div class=adn-url>' + keys[i] + "</div>\n";
-		var ads = data[keys[i]];
-
-		for (var m = 0, n = ads.length; m < n; m++) {
-			html += '<div class=adn-ad>';
-			html += "<a href='" + ads[m].target + "' target=_blank>\n";
-			html += "<img src='" + ads[m].url + "' title='" + ads[m].target + "'>\n";
-			html += "</a></div>\n";
-			//console.log(i+") "+ads[i].type+": "+ads[i].url+" :: "+ads[i].target);
-		}
-
-		html += "</div>\n";
-	}
-
 }

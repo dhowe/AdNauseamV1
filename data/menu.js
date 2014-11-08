85001cfe54b9a670e1c3868f68ca33c99d3890de
Mushon Zer-Aviv <mushon@shual.com>
2014-11-08 16:06:34 +0200

    better language for clicked / detected in menu

--- data/menu.js
+++ data/menu.js
@@ -35,7 +35,7 @@ self.port && self.port.on("load-advault", function() {
 function processAdData(adhash, pageUrl) {
 
 	var ads = toAdArray(adhash);
-	
+
 //console.warn("processAdData: "+ads.length+", "+pageUrl);
 
 	var ad, unique=0, onpage=[], soFar, hash = {};
@@ -55,8 +55,8 @@ function processAdData(adhash, pageUrl) {
 			ad.hidden = false;
 
 			// update count on this page
-			if (pageUrl === ads[i].pageUrl || 
-				(typeof testPageUrl != 'undefined' && 
+			if (pageUrl === ads[i].pageUrl ||
+				(typeof testPageUrl != 'undefined' &&
 					testPageUrl === ads[i].pageUrl))  // for testing
 			{
 				// TODO: don't count old ads from same url
@@ -85,19 +85,19 @@ function processAdData(adhash, pageUrl) {
 }
 
 function currentPage() {
-	
+
 	var url = window && window.top
 		 && window.top.getBrowser().selectedBrowser.contentWindow.location.href;
 	return url && url != "about:blank" ? url : null;
 }
 
 function updateAds(obj) {
-	
+
 	var adhash = obj.data, updates = obj.updates, page = obj.page;
-	
+
 	//console.log("updates: "+updates.length);
 	//console.log("ads: "+toAdArray(adhash).length);
-	
+
 	// change class, {title, (visitedTs) resolved}
 	for (var i=0, j = updates.length; i<j; i++) {
 
@@ -105,17 +105,17 @@ function updateAds(obj) {
 		var sel = '#ad' + updates[i].id + ' .title';
 		$(sel).text(updates[i].title);
 
-		// update the url		
+		// update the url
 		var sel = '#ad' + updates[i].id + ' cite';
 		$(sel).text(updates[i].resolvedTargetUrl);
-		
+
 		// update the class
 		$(sel).addClass('visited');
 	}
-	
+
 	var data = processAdData(adhash, page);
 	$('#visited-count').text(visitedCount(data.onpage)+' ads visited');
-	
+
 	//console.log('UPDATES COMPLETE!');
 	//setCounts(data.onpage.length, visitedCount, data.ads.length);
 }
@@ -124,7 +124,7 @@ function updateAds(obj) {
 function layoutAds(adHashAndPageObj) {
 
 	var adhash = adHashAndPageObj.data;
-	var page = typeof TEST_MODE != 'undefined' 
+	var page = typeof TEST_MODE != 'undefined'
 		&& TEST_MODE ? TEST_PAGE : adHashAndPageObj.page;
 	var data = processAdData(adhash, page);
 
@@ -135,31 +135,31 @@ function layoutAds(adHashAndPageObj) {
 	setCounts(data.onpage.length, visitedCount(data.onpage), data.ads.length);
 }
 
-function toAdArray(adhash, filter) { 
+function toAdArray(adhash, filter) {
 
 	var all = [], keys = Object.keys(adhash);
 	for (var i = 0, j = keys.length; i < j; i++) {
 
 		var ads = adhash[keys[i]];
 		for (var k=0; k < ads.length; k++) {
-			
-			if (!filter || filter(ads[k])) 
+
+			if (!filter || filter(ads[k]))
 				all.push(ads[k]);
 		}
 	}
-	
+
 	return all;
 }
 
 function setCounts(found, visited, total) {
-	
-	$('#found-count').text(found+' ads found');
-	$('#visited-count').text(visited+' ads visited');
+
+	$('#found-count').text(found+' ads detected');
+	$('#visited-count').text('clicked '+visited);
 	$('#vault-count').text(total);
 }
-	
+
 function visitedCount(arr) {
-	
+
 	var visitedCount = 0;
 	for (var i=0, j = arr.length; i<j; i++) {
 		if (!arr[i].hidden && arr[i].visitedTs > 0)
@@ -183,14 +183,14 @@ function createHtml(ads) {
 			html += '"><span class="thumb"><img src="' + ads[i].contentData;
 			html += '" class="ad-item-img"';// + visitedState(ads[i]);
 			html += ' onError="this.onerror=null; this.src=\'img/blank.png\';"';
-			html += '" alt="ad thumb"></span><span class="title">'; 
+			html += '" alt="ad thumb"></span><span class="title">';
 			html +=  ads[i].title ? ads[i].title  : "#"+ads[i].id;
 			html += '</span><cite>'+targetDomain(ads[i])+'</cite></a></li>\n\n';
 		}
 		else if (ads[i].contentType === 'text') {
 
 			html += '<li id="ad'+ads[i].id+'" class="ad-item-text'+visitedClass(ads[i]);
-			html += '""><span class="thumb">Text Ad</span><h3><a target="new" href="' 
+			html += '""><span class="thumb">Text Ad</span><h3><a target="new" href="'
 			html += ads[i].targetUrl + '">'+ads[i].title + '</a></h3><cite>' + targetDomain(ads[i]);
 			html += '</cite><div class="ads-creative">' + ads[i].contentData +'</div></li>\n\n';
 		}
@@ -203,13 +203,13 @@ function createHtml(ads) {
 
 function visitedClass(ad) {
 
-	return ad.visitedTs > 0 ? ' visited' : 
-		(ad.visitedTs < 0 ? ' errored' : '');  
+	return ad.visitedTs > 0 ? ' visited' :
+		(ad.visitedTs < 0 ? ' errored' : '');
 }
 
-/* 
+/*
  * Start with resolvedTargetUrl if available, else use targetUrl
- * Then extract the last domain from the (possibly complex) url 
+ * Then extract the last domain from the (possibly complex) url
  */
 function targetDomain(ad) {
 
@@ -219,7 +219,7 @@ function targetDomain(ad) {
 
 function extractDomains(text) {
 
-	var result = [], matches, 
+	var result = [], matches,
 		regexp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 
 	while (matches = regexp.exec(text))
@@ -240,33 +240,33 @@ function attachTests() {
 	$('#log-button').off('click').click(function() {
 		window.location.href = "log.html"
 	});
-	
+
 	$('#vault-button').off('click').click(function() {
 		window.location.href = "advault.html"
 	});
-	
+
 	$('#about-button').off('click').click(function() {
 		window.location.href = "https://github.com/dhowe/AdNauseam/wiki/Help"
-	}); 
-	 					 
+	});
+
 	$.getJSON(TEST_ADS, function(jsonObj) {
 
 		console.warn("Menu.js :: Loading test-ads: "+TEST_ADS);
 	    layoutAds({ data : jsonObj, page : TEST_PAGE });
-	    
+
 	}).fail(function(e) { console.warn( "error:", e); });
 }
-					
+
 (function() {
-		
-	console.log('INIT_HANDLERS');		
+
+	console.log('INIT_HANDLERS');
 
 	$('#log-button').click(function(e) {
 		//console.log('#log-button.click');
 
 		self.port && self.port.emit("show-log");
 	});
-	
+
 	$('#vault-button').click(function() {
 		//console.log('#vault-button.click');
 
@@ -274,17 +274,17 @@ function attachTests() {
 	});
 
 	$('#clear-ads').click(function(e) {
-		
+
 		e.preventDefault(); // no click
-		
+
 		// remove all visible ads from menu
 		$('.ad-item').remove();
 		$('.ad-item-text').remove();
 		setCounts(0, 0, 0);
-		
+
 		// trigger closing of settings
 		$("#settings-close").trigger( "click" );
-		
+
 		// call addon to clear simple-storage
 		self.port && self.port.emit("clear-ads");
 	});
@@ -315,18 +315,17 @@ function attachTests() {
 	});
 
 	$('#about-button').click(function() {
-		
+
 		//console.log('#about-button.click');
 		self.port && self.port.emit('show-about');
 	});
 
 	$('#cmn-toggle-1').click(function() {
-		
-		var val = $(this).prop('checked'); 
-		
+
+		var val = $(this).prop('checked');
+
 		//console.log('#disable-logs.click: '+val);
 		self.port && self.port.emit('disable-logs', { 'value' : val });
-	}); 
-	
-})();
+	});
 
+})();

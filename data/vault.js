15bc59852bc5f11f56693195c0f4d63e294efcb4
Daniel Howe <daniel@rednoise.org>
2015-01-06 13:13:14 +0800

    notes for positionAds

--- data/vault.js
+++ data/vault.js
@@ -730,8 +730,57 @@ function setZoom(idx) {
 	$('#ratio').html(zooms[idx]+'%');
 }
 
+// works for scale=1, needs to take other scales into account
+function bounds($items, scale) {
+    
+    var bounds = {
+        left: Number.POSITIVE_INFINITY,
+        top: Number.POSITIVE_INFINITY,
+        right: Number.NEGATIVE_INFINITY,
+        bottom: Number.NEGATIVE_INFINITY,
+        width: Number.NaN,
+        height: Number.NaN
+    };
+
+    $items.each(function (i,el) {
+        var elQ = $(el);
+        var off = elQ.offset();
+        off.right = off.left + $(elQ).width();
+        off.bottom = off.top + $(elQ).height();
+
+        if (off.left < bounds.left)
+        bounds.left = off.left;
+
+        if (off.top < bounds.top)
+        bounds.top = off.top;
+
+        if (off.right > bounds.right)
+        bounds.right = off.right;
+
+        if (off.bottom > bounds.bottom)
+        bounds.bottom = off.bottom;
+    });
+
+    bounds.width = bounds.right - bounds.left;
+    bounds.height = bounds.bottom - bounds.top;
+    
+    return bounds;
+}
+
+// Chooses the largest zoom size that fits at least 60% of each ad 
+function positionAds() { 
+    
+    var percentVisible = .6,
+        windowW = $("#container").width(),
+        windowH = $('#svgcon').offset().top;
+        
+    var boundsAll = bounds($('.item'));
+    // check if they fits at full zoom, 
+    // if so return; if not, compute with scale
+} 
+
 // TODO: broken (see issue #59) -- needs to be rewritten
-function positionAds() { // autozoom & center 
+function positionAdsOld() { // autozoom & center 
 
 	var percentVisible = .6,
 		winW = $("#container").width(),

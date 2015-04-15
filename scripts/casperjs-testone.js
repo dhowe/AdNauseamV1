bb17f646d260ff75361bb54e2a9473ba85c77d8f
Daniel Howe <daniel@rednoise.org>
2015-04-15 11:45:56 +0800

    replaced full tests in travis.yml

--- scripts/casperjs-testone.js
+++ scripts/casperjs-testone.js
@@ -1,17 +1,18 @@
-
 /*global casper */
 
 var userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:36.0) Gecko/20100101 Firefox/36.0',
-    selector = '#tads.c', name = 'adsense-1', timeoutMs = 10000,
-    url = 'https://www.google.com/search?q=apple&ie=utf-8&oe=utf-8';
+    url = 'https://www.google.com/search?q=apple&ie=utf-8&oe=utf-8',
+    selector = '#tads.c',
+    timeoutMs = 10000;
+    
 
-casper.test.begin(name, 1, function(test) {
+casper.test.begin("test1", 1, function(test) {
 
         casper.userAgent(userAgent);
 
         casper.start(url)
-        
-            .waitForSelector(selector, function() {
+
+        .waitForSelector(selector, function() {
 
                 test.assertExists(selector, 'selector: ' + selector);
 
@@ -21,7 +22,7 @@ casper.test.begin(name, 1, function(test) {
 
             }, timeoutMs)
 
-            .run(function() {
+        .run(function() {
 
                 test.done();
             });

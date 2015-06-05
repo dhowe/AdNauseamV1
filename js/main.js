b79bf076e536a642b1b8cbf7aaebdaca7ecabf03
cqx931 <cqx931@gmail.com>
2015-06-06 02:36:21 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+	var original;
+    $("#logo.subpage").mouseover(function(){
+    	original= $(this).attr('src');
+        $(this).attr('src','img/adn_animated_croped.png');
+    }); 
+    $("#logo.subpage").mouseout(function(){
+        $(this).attr('src',original);
+    });
+});

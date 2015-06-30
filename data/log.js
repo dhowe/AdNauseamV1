63e0c522db3bc245fa10dd7baf28084f27580c25
CyrusSUEN <yuyhero@gmail.com>
2015-07-01 04:26:15 +0800

    Reverse log entries and Fixed close log bug

--- data/log.js
+++ data/log.js
@@ -1,18 +1,46 @@
 self.port && self.port.on('show-log', function(payLoad) {
 	
-	// console.log("logJSON from log.js: ", payLoad);
-	printLog(payLoad);
+	initLog(payLoad);
+});
+self.port && self.port.on('add-log-entry', function(payLoad) {
+	
+	addLogEntry(payLoad);
 });
 
-function printLog(log) {
+function initLog(log) {
 	
 	for (var i in log) {
 		
 		if ( i == 0 && ( log[i].i == 0 ) )
 			$('#content').html("");
 			
-		$('#content').append( log[i].msg + "</br>" );
+		var $entry = $('<span>', {
+
+			text: deleteExtraChars(log[i].msg)
+		}).append("</br>");
 		
-		window.scrollTo(0, document.body.scrollHeight);
+		$('#content').prepend( $entry );
 	}
+}
+
+function deleteExtraChars(text) {
+	
+	// needs better handling of the Option entry
+	return text.replace(/\n(.*)/g, "");
+}
+
+function addLogEntry(log) {
+	
+	// delete excessive log entry
+	if ( $('#content').children().length > 100 )
+		$('#content').find(":first-child").remove();
+
+	var $entry = $('<span>', {
+
+		text: deleteExtraChars(log[log.length - 1].msg)
+	}).append("</br>").hide();
+
+	$('#content').prepend($entry);
+	
+	$entry.fadeIn(2500);
 }

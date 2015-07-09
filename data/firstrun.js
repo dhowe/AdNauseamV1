fa49bec05242fb0221c799fb474ec5d1842da028
Daniel Howe <daniel@rednoise.org>
2015-07-09 19:44:24 +0800

    fix to #384 and various cleanup

--- data/firstrun.js
+++ data/firstrun.js
@@ -1,21 +1,21 @@
  /*global window:0, document:0, self:0 */
 
  $(function() {
-     
-     $("#version").text('v'+self.options.version);
-    
-    $('#x-close-button').click(function() {
-            
-        self.port && self.port.emit("close-firstrun");
-    });
-    
-    $('#start').click(function() {
-            
-        self.port && self.port.emit("close-firstrun");
-    });
-    
-    $('#video').click(function() {
-            
-        window.open('https://vimeo.com/111943439');
-    });
-});
+
+   $("#version").text('v' + self.options.version);
+
+   $('#x-close-button').click(function() {
+
+     self.port && self.port.emit("close-firstrun");
+   });
+
+   $('#start').click(function() {
+
+     self.port && self.port.emit("close-firstrun");
+   });
+
+   $('#video').click(function() {
+
+     window.open('https://vimeo.com/111943439');
+   });
+ });

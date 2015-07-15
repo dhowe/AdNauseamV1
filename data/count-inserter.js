d3ffa2cbbf80d6f6795a9a6279f95e9988a75346
Daniel Howe <daniel@rednoise.org>
2015-07-15 17:40:30 +0800

    temporary fix to #401 and incorrect version in settings

--- data/count-inserter.js
+++ data/count-inserter.js
@@ -1,10 +1,12 @@
 
 /*global self */
-self.port && self.port.on('insert-count', function(json) { 
+self.port && self.port.on('insert-count', function(json) {
     var adndiv = $('#adnauseam-count');
-    if (!adndiv.length) 
-        $('body').append('<div id="adnauseam-count" count="1"/>');
+    if (!adndiv.length) {
+        $('body').append('<div id="adnauseam-count" count="0"/>');
+        console.log("Inject: #adnauseam-count");
+    }
     adndiv.attr('count', json.count);
-    //console.log("adndiv.attr('count', "+json.count+")");
+    console.log("adndiv.attr('count', "+json.count+")");
     console.log("INSERT_COUNT("+json.count+")="+$('#adnauseam-count').attr('count'));
 });
